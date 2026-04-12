d45f8729c435cec656607e428ef168df83c82170
rayala <rayalanagendra4545@gmail.com>
2026-04-12 13:51:18 +0530

    updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+baseURL: "https://usermanagementsystem1-production.up.railway.app",
 });
 
 axiosInstance.interceptors.request.use((config) => {
